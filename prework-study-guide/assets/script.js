e2f834514a469f97fd8935257e5f8a6efbb52d4e
jonathAnC0bosR <jonathan.cobos@outlook.es>
2023-02-20 00:11:03 -0600

    Added Javascript to the main code

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+
+/*
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let´s study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let´s study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let´s study Git!");
+} else if (topic === 'Javascrpit') {
+    console.log("Let´s study Javascript!");
+} else {
+    console.log('Please try again!');
+}
+*/
